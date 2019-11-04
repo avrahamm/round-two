37bf5c4203181ca76cbbb93db88ea7040934e75b
Ilya Gelman <ilyagelman@ilyagelman.com>
2019-11-04 16:54:27 +0200

    add assignment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,36 @@ import React from 'react';
 
 const App = () => (
   <React.Fragment>
-    Good luck!
+    <input placeholder="filter" type="text"/>
+
+    <div className="node">
+      <input type="checkbox" checked/>
+      <span>North America</span>
+
+      <div className="children">
+        <div className="node">
+          <input type="checkbox" checked/>
+          <span>Canada</span>
+
+          <div className="children">
+            <div className="node">
+              <input type="checkbox" checked/>
+              <span>Montreal</span>
+            </div>
+
+            <div className="node">
+              <input type="checkbox"/>
+              <span>Toronto</span>
+            </div>
+
+            <div className="node">
+              <input type="checkbox" checked/>
+              <span>Ottawa</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </React.Fragment>
 );
 
